4acf6ec11ae8c11f0fc49a7174e2e3c690bc58de
RomanTikhonyuk <164060911+RomanTikhonyuk@users.noreply.github.com>
2024-06-20 11:25:55 +0300

    Upd

--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -9,7 +9,7 @@ import java.util.Optional;
 
 @Service
 public class UserService {
-    UserRepository userRepository;
+    private final UserRepository userRepository;
 
     @Autowired
     public UserService(UserRepository userRepository) {
